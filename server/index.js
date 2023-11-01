0d812846ef1ba13ec9c4d2557b116aad74a6e3a7
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-01 16:14:25 +0700

    fix : removing double dotenv config on index

--- server/index.js
+++ server/index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const dotenv =  require('dotenv');
+const express = require("express");
+require("dotenv").config();
 
-const connectDB = require('./database/connection');
+const connectDB = require("./database/connection");
 
-const bookRouter = require('./router/bookRouter');
-const transactionRouter = require('./router/transactionRouter');
+const bookRouter = require("./router/bookRouter");
+const transactionRouter = require("./router/transactionRouter");
 
 const app = express();
 
-
 app.use(express.json()); // Middleware untuk parsing JSON requests
-app.use('/books', bookRouter); // Menggunakan bookRouter untuk endpoint '/books'
-app.use('/transactions', transactionRouter); // Menggunakan transactionRouter untuk endpoint '/transactions'
+app.use("/books", bookRouter); // Menggunakan bookRouter untuk endpoint '/books'
+app.use("/transactions", transactionRouter); // Menggunakan transactionRouter untuk endpoint '/transactions'
 
-const errorHandlerMiddleware = require('./middleware/errorHandlerMiddleware');
+const errorHandlerMiddleware = require("./middleware/errorHandlerMiddleware");
 app.use(errorHandlerMiddleware);
 
-
-dotenv.config({path:'config.env'})
 const PORT = process.env.port || 8000;
 
 connectDB();
 
-app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
