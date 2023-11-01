adc80185a6ecbd80f7349055bbe6e039bdf9dc79
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-01 16:14:50 +0700

    update : add default value for employeemodel

--- server/models/employeeModel.js
+++ server/models/employeeModel.js
@@ -1,26 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const employeeSchema = new mongoose.Schema({
-    id: {
-        type: String, 
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Employee', employeeSchema);
+// Middleware pre-save untuk mengatur increment ID
+employeeSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    // Hanya berjalan saat dokumen baru dibuat
+    const lastEmployee = await this.constructor.findOne().sort({ id: -1 }); // Dapatkan karyawan dengan ID terbesar
+    if (lastEmployee) {
+      const lastEmployeeIdNumber = parseInt(lastEmployee.id.substring(1)); // Dapatkan angka dari ID karyawan (abaikan huruf C)
+      this.id = "C" + (lastEmployeeIdNumber + 1); // Buat ID baru dengan menambahkan 1 ke ID sebelumnya
+    } else {
+      this.id = "C1"; // Jika belum ada karyawan, set ID ke C1
+    }
+  }
+  next();
+});
+
+module.exports = mongoose.model("Employee", employeeSchema);
