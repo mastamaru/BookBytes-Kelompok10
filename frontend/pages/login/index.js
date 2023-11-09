70c14eeed8ac19ffda8f24121d7bb12a927ce216
rhafif-p <rhafif.prasetyo@mail.ugm.ac.id>
2023-11-09 22:27:55 +0700

    fix: minor styling and add routing

--- frontend/pages/login/index.js
+++ frontend/pages/login/index.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
-import validation from './LoginValidation';
 import Image from "next/image";
-
+import { useRouter } from 'next/router';
 
 
 
 
 export default function LoginSignup(){
+    const router = useRouter();
     const[values, setValues] = useState({
         username : "",
         password:""
     })
 
-    const [errors,setErrors] = useState({})
+    const [msg,SetMSG] = useState()
 
     const handleInput = (event) => {
         const { name, value } = event.target;
@@ -23,7 +23,6 @@ export default function LoginSignup(){
 
     const handleLogin = async (event) => {
         event.preventDefault();
-        setErrors(validation(values));
     
         try {
             const response = await fetch("http://localhost:8000/employee/login", {
@@ -36,10 +35,20 @@ export default function LoginSignup(){
     
             const result = await response.json();
     
-            if (result === "success") {
-                console.log("Success");
-            } else {
-                console.log(result);
+            if (result === "Cashier" || result === "cashier") {
+                console.log("Login Success");
+                SetMSG();
+                
+                
+            }
+            else if (result === "admin" || result === "Admin") {
+                console.log("admin");
+                SetMSG();
+                router.push("/admin/transaksi");
+
+            }  
+            else {
+                SetMSG(result);
             }
         } catch (error) {
             console.error("Error:", error);
@@ -51,14 +60,14 @@ export default function LoginSignup(){
     return(
         <section className="body bg-[url('/assets/bglogin.png')] relative h-[100vh] bg-cover 
         flex flex-col items-center justify-center h-screen">
-            <div class="flex flex-col  items-center max-w-full bg-white rounded-lg p-14">
+            <div class="flex flex-col  items-center max-w-full bg-white rounded-[25px] p-14">
                 <Image
                 src={"/assets/logo.png"}
-                width={150}
+                width={180}
                 height={100}
                 alt="logo"
                 />
-                <h1 className="font-mplus text-center text-black text-[40px] font-bold leading-normal pb-7 pt-6 px-12">
+                <h1 className="font-mplus text-center text-black text-[40px] font-bold leading-normal pb-7 pt-6 px-10">
                 Masuk dengan Akun Terdaftar
                 </h1>
                 <div className="inputs flex flex-col gap-7 pb-2">
@@ -68,7 +77,7 @@ export default function LoginSignup(){
                         placeholder="Username"
                         onChange={handleInput}
                         name="username"
-                        className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent "
+                        className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent text-xl "
                         />
                     </div>
                     <div className="input flex items-center">
@@ -77,17 +86,18 @@ export default function LoginSignup(){
                         placeholder="Kata Sandi"
                         onChange={handleInput}
                         name="password"
-                        className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent"
+                        className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent text-xl"
                         />
                     </div>
                 </div>
+                {msg && <span className="text-red-500 text-lg font-roboto ">{msg}</span>}
                 <div className='pt-5'>
                     <button onClick={handleLogin} className="rounded-lg border bg-blue-700 text-white font-bold text-lg py-4 px-8">  
                     Masuk
                     </button>
                 </div>
-                <button className="self-start mt-auto text-black text-lg font-bold font-mplus-bold bg-transparent">
-                &#60; Kembali
+                <button className="self-start mt-auto text-black text-xl font-bold font-mplus-bold bg-transparent">
+                &#60;  Kembali
                 </button>
 
             
