564336e132ae1d2e3b407928d78b306f582373f5
mailbau <simailpipiw@gmail.com>
2023-11-14 14:39:54 +0700

    button positioning

--- frontend/pages/admin/buku/index.js
+++ frontend/pages/admin/buku/index.js
@@ -45,75 +45,77 @@ export default function Buku() {
             selectedLabel="Data Buku"
             className="left-[22%] top-[7%]"
           />
-          <div className="flex flex-col items-center ">
-            <h1 className="font-mplus font-medium text-[65px] ">Data Buku</h1>
-            <Button
-              text="+ Tambah Buku"
-              color="blue"
-              className="py-2 px-4 text-[24px] absolute left-[24%] top-[37%]"
-              onClick={() => setIsModalOpen(true)}
+          <div className="w-[1200px] mx-auto">
+            <div className="flex flex-col items-center ">
+              <h1 className="font-mplus font-medium text-[65px] ">Data Buku</h1>
+              <Button
+                text="+ Tambah Buku"
+                color="blue"
+                className="py-2 px-4 text-[24px] mr-auto ml-[100px]"
+                onClick={() => setIsModalOpen(true)}
 
-            />
-            <AddBookModal
-              isOpen={isModalOpen}
-              onClose={() => setIsModalOpen(false)}
-              onAddBook={handleAddBook}
-            />
-            <div
-              className="table-container mt-12 max-h-[55vh] overflow-y-scroll"
-              style={{ maxWidth: "1100px", marginLeft: "100px" }}
-            >
-              <table style={{ tableLayout: "fixed", width: "100%" }}>
-                <colgroup>
-                  <col style={{ width: "10%" }} />
-                </colgroup>
-                <thead>
-                  <tr>
-                    <th style={{ fontSize: "16px" }}>ID Buku</th>
-                    <th style={{ fontSize: "16px" }}>Judul Buku</th>
-                    <th style={{ fontSize: "16px" }}>Pengarang</th>
-                    <th style={{ fontSize: "16px" }}>Penerbit</th>
-                    <th style={{ fontSize: "16px" }}>Tahun Terbit</th>
-                    <th style={{ fontSize: "16px" }}>Genre</th>
-                    <th style={{ fontSize: "16px" }}>Harga</th>
-                    <th style={{ fontSize: "16px" }}>Aksi</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {books.map((book) => (
-                    <tr key={book.bookID}>
-                      <td>{book.bookID}</td>
-                      <td>{book.title}</td>
-                      <td>{book.author}</td>
-                      <td>{book.publisher}</td>
-                      <td>{book.year}</td>
-                      <td>{book.genre}</td>
-                      <td>{book.price}</td>
-                      <td
-                        style={{
-                          display: "flex",
-                          justifyContent: "space-evenly",
-                          alignItems: "center",
-                          width: "100%",
-                        }}
-                      >
-                        <button onClick={() => handleEditBook(book.bookID)}>
-                          <FontAwesomeIcon
-                            icon={faEdit}
-                            style={{ color: "blue" }}
-                          />
-                        </button>
-                        <button onClick={() => handleDeleteBook(book.bookID)}>
-                          <FontAwesomeIcon
-                            icon={faTrash}
-                            style={{ color: "red" }}
-                          />
-                        </button>
-                      </td>
+              />
+              <AddBookModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onAddBook={handleAddBook}
+              />
+              <div
+                className="table-container mt-12 max-h-[55vh] overflow-y-scroll"
+                style={{ maxWidth: "1100px", marginLeft: "100px" }}
+              >
+                <table style={{ tableLayout: "fixed", width: "100%" }}>
+                  <colgroup>
+                    <col style={{ width: "10%" }} />
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th style={{ fontSize: "16px" }}>ID Buku</th>
+                      <th style={{ fontSize: "16px" }}>Judul Buku</th>
+                      <th style={{ fontSize: "16px" }}>Pengarang</th>
+                      <th style={{ fontSize: "16px" }}>Penerbit</th>
+                      <th style={{ fontSize: "16px" }}>Tahun Terbit</th>
+                      <th style={{ fontSize: "16px" }}>Genre</th>
+                      <th style={{ fontSize: "16px" }}>Harga</th>
+                      <th style={{ fontSize: "16px" }}>Aksi</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {books.map((book) => (
+                      <tr key={book.bookID}>
+                        <td>{book.bookID}</td>
+                        <td>{book.title}</td>
+                        <td>{book.author}</td>
+                        <td>{book.publisher}</td>
+                        <td>{book.year}</td>
+                        <td>{book.genre}</td>
+                        <td>{book.price}</td>
+                        <td
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-evenly",
+                            alignItems: "center",
+                            width: "100%",
+                          }}
+                        >
+                          <button onClick={() => handleEditBook(book.bookID)}>
+                            <FontAwesomeIcon
+                              icon={faEdit}
+                              style={{ color: "blue" }}
+                            />
+                          </button>
+                          <button onClick={() => handleDeleteBook(book.bookID)}>
+                            <FontAwesomeIcon
+                              icon={faTrash}
+                              style={{ color: "red" }}
+                            />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
