9a821f69762ed50655dda5a098914e6e927e1686
Thoriqutsani Anastyo Rasnan <74649373+goodguythor@users.noreply.github.com>
2023-11-16 22:37:23 +0700

    fix: update cashier transaksi page

--- frontend/pages/cashier/transaksi/index.js
+++ frontend/pages/cashier/transaksi/index.js
@@ -7,41 +7,64 @@ import "moment-timezone";
 import AddRowModal from "@/lib/addRowModal";
 import NavbarAdmin from "@/components/NavbarAdmin";
 import Button from "@/components/Button";
-import { data } from "autoprefixer";
 import { fetchTransactions } from "@/lib/getTransaction";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import DeleteRowModal from "@/lib/delRowModal";
+
 export default function Kasir() {
-  const [employees, setTransactions] = useState([]);
+  const [employees, setEmployees] = useState([]);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const router = useRouter();
+  var ind;
+
   useEffect(() => {
-    //add token validation to check user session
+    // Add token validation to check the user session
     const token = localStorage.getItem("token");
 
     const isTokenValid = () => {
       return token != null;
     };
 
-    if (!isTokenValid()) {  
+    if (!isTokenValid()) {
       router.push("/login");
     }
 
-    const getTransaction = async () => {
+    const getEmployees = async () => {
       try {
         const data = await fetchEmployees();
-        console.log("Fetched Transactions:", data);
-        setTransactions(data);
+        console.log("Fetched Employees:", data);
+        setEmployees(data);
       } catch (error) {
         console.error(error);
       }
     };
 
-    getTransaction();
+    getEmployees();
   }, []);
 
   const [rows, setRows] = useState([]);
 
   const handleAddRow = (newRow) => {
     setRows([...rows, newRow]);
+  };
+
+  const handleDeleteRow = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const confirmDeleteRow = (index) => {
+    ind = index;
+    try {
+      // Perform the deletion logic here, using the provided index
+      const updatedRows = [...rows];
+      updatedRows.splice(index, 1); // Remove the row at the specified index
+      setRows(updatedRows);
+      setIsDeleteModalOpen(false);
+    } catch (error) {
+      console.error("Error deleting row", error);
+    }
   };
 
   // Fungsi untuk menangani proses logout
@@ -64,13 +87,12 @@ export default function Kasir() {
           alt="logo"
         />
         <div className="flex flex-row items-end gap-10 absolute top-[0] left-[35%] leading-normal font-mplus font-bold text-[35px]">
-            <div
-                className="rounded-[15px] px-[92px] py-6 bg-[#FBB91C] mx-auto"
-                alt="transaksi"
-            >
-                Transaksi
-            </div>
-
+          <div
+            className="rounded-[15px] px-[92px] py-6 bg-[#FBB91C] mx-auto"
+            alt="transaksi"
+          >
+            Transaksi
+          </div>
         </div>
         <div className="pt-[200px] relative">
           <div className="flex flex-col items-center ">
@@ -83,6 +105,7 @@ export default function Kasir() {
                     <th>Jumlah</th>
                     <th>Harga Satuan</th>
                     <th>Harga</th>
+                    <th>Aksi</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -93,6 +116,15 @@ export default function Kasir() {
                       <td>{row.quantity}</td>
                       <td>{row.price}</td>
                       <td>{row.quantity * row.price}</td>
+                      <td>
+                        <button onClick={() => handleDeleteRow(index)}>
+                          <FontAwesomeIcon
+                            icon={faTrash}
+                            className="py-2 px-4 text-[24px] items-center relative top-[10px]"
+                            style={{ color: "red" }}
+                          />
+                        </button>
+                      </td>
                     </tr>
                   ))}
                 </tbody>
@@ -105,8 +137,8 @@ export default function Kasir() {
               />
               <AddRowModal
                 isOpen={isAddModalOpen}
-                onAddRow={handleAddRow}
                 onClose={() => setIsAddModalOpen(false)}
+                onAddRow={handleAddRow}
               />
               <Button
                 color={"green"}
@@ -115,24 +147,28 @@ export default function Kasir() {
               />
             </div>
           </div>
-        </div> 
+        </div>
+        {/* Move DeleteRowModal outside the table */}
+        <DeleteRowModal
+          isOpen={isDeleteModalOpen}
+          onClose={() => setIsDeleteModalOpen(false)}
+          onDeleteRow={() => confirmDeleteRow(ind)}
+        />
         <div className="flex flex-row items-end gap-6 absolute top-[11%] left-[67.5%] leading-normal font-mplus font-bold text-[35px]">
-            <h1>
-                Kasir:
-            </h1>
-            <h1 className="text-[#1820EF]">
-                {employees.map((employee) => (
-                    <span key={employee.id}>{employee.name}</span>
-                ))}
-            </h1>
-            <Button
-              onClick={handleLogout}
-                icon={"/assets/signout.svg"}
-                text={"Keluar"}
-                color="red"
-                className="flex py-1 px-5 text-sm items-center relative top-[-10px]"
-            />
-        </div> 
+          <h1>Kasir:</h1>
+          <h1 className="text-[#1820EF]">
+            {employees.map((employee) => (
+              <span key={employee.id}>{employee.name}</span>
+            ))}
+          </h1>
+          <Button
+            onClick={handleLogout}
+            icon={"/assets/signout.svg"}
+            text={"Keluar"}
+            color="red"
+            className="flex py-1 px-5 text-sm items-center relative top-[-10px]"
+          />
+        </div>
       </section>
     </>
   );
