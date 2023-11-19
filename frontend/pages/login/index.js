8ec6f8082a42bd3de5f04741015bf9c148f39836
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-19 21:33:35 +0700

    fix: height screen

--- frontend/pages/login/index.js
+++ frontend/pages/login/index.js
@@ -39,8 +39,8 @@ export default function LoginSignup() {
         if (result.role.toLowerCase() === "cashier") {
           console.log("Login Success: Cashier");
           SetMSG("Login Success: Cashier");
-          router.push("cashier/transaksi")
-          
+          router.push("cashier/transaksi");
+
           // Redirect to cashier's page if needed
         } else if (result.role.toLowerCase() === "admin") {
           console.log("Login Success: Admin");
@@ -70,54 +70,57 @@ export default function LoginSignup() {
   };
 
   return (
-    <section
-      className="body bg-[url('/assets/bglogin.png')] relative h-[100vh] bg-cover 
-        flex flex-col items-center justify-center h-screen"
-    >
-      <div className="flex flex-col  items-center max-w-full bg-white rounded-[25px] p-14">
-        <Image src={"/assets/logo.png"} width={180} height={100} alt="logo" />
-        <h1 className="font-mplus text-center text-black text-[40px] font-bold leading-normal pb-7 pt-6 px-10">
-          Masuk dengan Akun Terdaftar
-        </h1>
-        <div className="inputs flex flex-col gap-7 pb-2">
-          <div className="input flex items-center font-roboto font-normal">
-            <input
-              type="nama"
-              placeholder="Username"
-              onKeyDown={handleKeyDown}
-              onChange={handleInput}
-              name="username"
-              className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent text-xl "
-            />
+    <>
+      <section
+        className="body bg-[url('/assets/bglogin.png')] relative h-[100vh] bg-cover 
+      flex flex-col items-center justify-center"
+      >
+        <div className="flex flex-col  items-center max-w-full bg-white rounded-[25px] p-14">
+          <Image src={"/assets/logo.png"} width={180} height={100} alt="logo" />
+          <h1 className="font-mplus text-center text-black text-[40px] font-bold leading-normal pb-7 pt-6 px-10">
+            Masuk dengan Akun Terdaftar
+          </h1>
+          <div className="inputs flex flex-col gap-7 pb-2">
+            <div className="input flex items-center font-roboto font-normal">
+              <input
+                type="nama"
+                placeholder="Username"
+                onKeyDown={handleKeyDown}
+                onChange={handleInput}
+                name="username"
+                className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent text-xl "
+              />
+            </div>
+            <div className="input flex items-center">
+              <input
+                type="password"
+                onKeyDown={handleKeyDown}
+                placeholder="Kata Sandi"
+                onChange={handleInput}
+                name="password"
+                className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent text-xl"
+              />
+            </div>
           </div>
-          <div className="input flex items-center">
-            <input
-              type="password"
-              onKeyDown={handleKeyDown}
-              placeholder="Kata Sandi"
-              onChange={handleInput}
-              name="password"
-              className="w-96 h-14 px-4 border border-gray-300 rounded-md bg-transparent text-xl"
-            />
+          {msg && (
+            <span className="text-red-500 text-lg font-roboto ">{msg}</span>
+          )}
+          <div className="pt-5">
+            <button
+              onClick={handleLogin}
+              className="rounded-lg border bg-blue-700 text-white font-bold text-lg py-4 px-8"
+            >
+              Masuk
+            </button>
           </div>
-        </div>
-        {msg && (
-          <span className="text-red-500 text-lg font-roboto ">{msg}</span>
-        )}
-        <div className="pt-5">
           <button
-            onClick={handleLogin}
-            className="rounded-lg border bg-blue-700 text-white font-bold text-lg py-4 px-8"
+            onClick={() => router.push("/")}
+            className="self-start mt-auto text-black text-xl font-bold font-mplus-bold bg-transparent"
           >
-            Masuk
+            &#60; Kembali
           </button>
         </div>
-        <button 
-        onClick = {() => router.push('/')}
-        className="self-start mt-auto text-black text-xl font-bold font-mplus-bold bg-transparent">
-          &#60; Kembali
-        </button>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
