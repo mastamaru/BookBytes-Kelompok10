884e9c05f343603855ce08902f6f47d974033937
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-28 21:18:25 +0700

    change : delete alert content

--- frontend/lib/FunctionEmployee.js
+++ frontend/lib/FunctionEmployee.js
@@ -1,35 +1,34 @@
 const API_URL = "http://localhost:8000";
 
 export const fetchEmployees = async () => {
-    try {
-        const employeeResponse = await fetch(`${API_URL}/employee`);
-        if (!employeeResponse.ok) {
-            throw new Error("Network response was not ok for employee");
-        }
-        const employeesData = (await employeeResponse.json()).data; // Perubahan disini
-
-        return employeesData;
-    } catch (error) {
-        console.error("Error fetching employee:", error);
-        throw error;
+  try {
+    const employeeResponse = await fetch(`${API_URL}/employee`);
+    if (!employeeResponse.ok) {
+      throw new Error("Network response was not ok for employee");
     }
+    const employeesData = (await employeeResponse.json()).data; // Perubahan disini
+
+    return employeesData;
+  } catch (error) {
+    console.error("Error fetching employee:", error);
+    throw error;
+  }
 };
 
-export const delEmployees = async(employId) => {
-    try {
-        const response = await fetch(`${API_URL}/employee/${employId}`,{
-        method: "DELETE",
+export const delEmployees = async (employId) => {
+  try {
+    const response = await fetch(`${API_URL}/employee/${employId}`, {
+      method: "DELETE",
     });
 
-    if (response.ok){
-        alert("Book sucsessfully deleted");
+    if (response.ok) {
+      alert("Employee Deleted Successfully!");
+    } else {
+      const errorData = await response.json();
+      alert("Fail");
+      console.log(errorData);
     }
-    else{
-        const errorData = await response.json();
-        alert("Fail")
-        console.log(errorData)
-    }
-    } catch (error){
-        console.error("Error deleting book:", error);
-    }
+  } catch (error) {
+    console.error("Error deleting book:", error);
+  }
 };
