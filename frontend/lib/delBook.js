241147e4284ccb44ccb46f975016ce15a16564cc
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-30 16:07:53 +0700

    change: Update API URL to use Vercel deployment

--- frontend/lib/delBook.js
+++ frontend/lib/delBook.js
@@ -1,19 +1,19 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://book-bytes-kelompok10.vercel.app";
 
 export const delBook = async (bookID) => {
-    try {
-        const response = await fetch(`${API_URL}/books/${bookID}`, {
-            method: "DELETE",
-        });
+  try {
+    const response = await fetch(`${API_URL}/books/${bookID}`, {
+      method: "DELETE",
+    });
 
-        if (!response.ok) {
-            throw new Error("Failed to delete book");
-        }
-
-        const deletedBook = await response.json();
-        return deletedBook;
-    } catch (error) {
-        console.error("Error deleting book:", error);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Failed to delete book");
     }
+
+    const deletedBook = await response.json();
+    return deletedBook;
+  } catch (error) {
+    console.error("Error deleting book:", error);
+    throw error;
+  }
 };
