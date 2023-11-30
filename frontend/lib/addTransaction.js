171b49d717fcd0f2e6cca10832304294ce937735
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-30 16:07:38 +0700

    change Update API URL to use Vercel deployment

--- frontend/lib/addTransaction.js
+++ frontend/lib/addTransaction.js
@@ -1,23 +1,23 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://book-bytes-kelompok10.vercel.app";
 
 export const addTransaction = async (newTransaction) => {
-    try {
-        const response = await fetch(`${API_URL}/transactions`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newTransaction),
-        });
+  try {
+    const response = await fetch(`${API_URL}/transactions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTransaction),
+    });
 
-        if (!response.ok) {
-            throw new Error("Failed to add transaction");
-        }
-
-        const addedTransaction = await response.json();
-        return addedTransaction;
-    } catch (error) {
-        console.error("Error adding transaction:", error);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Failed to add transaction");
     }
+
+    const addedTransaction = await response.json();
+    return addedTransaction;
+  } catch (error) {
+    console.error("Error adding transaction:", error);
+    throw error;
+  }
 };
