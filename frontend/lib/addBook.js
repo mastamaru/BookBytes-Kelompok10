2b52eaee6bf331a890066c17cc1232193d90f9c4
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-30 16:07:30 +0700

    change Update API URL to use Vercel deployment

--- frontend/lib/addBook.js
+++ frontend/lib/addBook.js
@@ -1,23 +1,23 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://book-bytes-kelompok10.vercel.app";
 
 export const addBook = async (newBook) => {
-    try {
-        const response = await fetch(`${API_URL}/books`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newBook),
-        });
+  try {
+    const response = await fetch(`${API_URL}/books`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBook),
+    });
 
-        if (!response.ok) {
-            throw new Error("Failed to add book");
-        }
-
-        const addedBook = await response.json();
-        return addedBook;
-    } catch (error) {
-        console.error("Error adding book:", error);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Failed to add book");
     }
+
+    const addedBook = await response.json();
+    return addedBook;
+  } catch (error) {
+    console.error("Error adding book:", error);
+    throw error;
+  }
 };
