260eed2fe14116b803787fafb34042d870ad1068
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-11-30 16:08:04 +0700

    change: Update API URL to use Vercel deployment

--- frontend/lib/getEmployee.js
+++ frontend/lib/getEmployee.js
@@ -1,17 +1,16 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://book-bytes-kelompok10.vercel.app";
 
 export const fetchEmployees = async () => {
-    try {
-        const employeeResponse = await fetch(`${API_URL}/employee`);
-        if (!employeeResponse.ok) {
-            throw new Error("Network response was not ok for employee");
-        }
-        const employeesData = (await employeeResponse.json()).data; // Perubahan disini
-
-        return employeesData;
-    } catch (error) {
-        console.error("Error fetching employee:", error);
-        throw error;
+  try {
+    const employeeResponse = await fetch(`${API_URL}/employee`);
+    if (!employeeResponse.ok) {
+      throw new Error("Network response was not ok for employee");
     }
-};
+    const employeesData = (await employeeResponse.json()).data; // Perubahan disini
 
+    return employeesData;
+  } catch (error) {
+    console.error("Error fetching employee:", error);
+    throw error;
+  }
+};
