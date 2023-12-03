2ee44d26cff3beec9b5d4c098c9c3b8662130408
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-12-03 19:38:52 +0700

    change : moving modal to component folder

---
+++ frontend/components/addBookModal.js
@@ -0,0 +1,256 @@
+// AddBookModal.js
+import React, { useState } from "react";
+import Button from "@/components/Button";
+import { addBook } from "@/lib/addBook";
+
+const predefinedGenres = [
+  "Fiksi",
+  "Non Fiksi",
+  "Fiksi Ilmiah",
+  "Misteri",
+  "Fantasi",
+  "Drama",
+  "Horror",
+  "Romansa",
+];
+
+const modalOverlayStyle = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  background: "rgba(0, 0, 0, 0.5)",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const modalPopupStyle = {
+  background: "#fff",
+  borderRadius: "8px",
+  boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+  maxWidth: "400px",
+  width: "100%",
+  maxHeight: "70vh", // Set your desired maximum height
+  overflowY: "auto", // Enable vertical scrolling if content overflows
+  padding: "20px",
+};
+
+const buttonContainerStyle = {
+  marginTop: "20px", // Adjust the margin to control the spacing
+  display: "flex",
+  justifyContent: "flex-end", // Align buttons to the end of the container
+};
+
+const buttonStyle = {
+  borderRadius: "8px",
+  padding: "10px 20px",
+  cursor: "pointer",
+};
+
+const cancelButtonStyle = {
+  ...buttonStyle,
+  marginRight: "10px", // Adjust the margin between buttons
+  background: "#fd4d3e",
+  color: "#fff",
+};
+
+const submitButtonStyle = {
+  ...buttonStyle,
+  background: "#18ef61",
+  color: "#fff",
+};
+
+const labelStyle = {
+  marginBottom: "5px",
+  fontweight: "bold",
+};
+
+const inputContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  marginBottom: "15px",
+};
+
+const inputStyle = {
+  width: "100%", // Set to your desired width
+  padding: "10px",
+  borderRadius: "8px",
+  boxSizing: "border-box",
+  border: "1px solid #ccc",
+};
+
+const AddBookModal = ({ isOpen, onClose, onAddBook }) => {
+  const [newBook, setNewBook] = useState({
+    bookID: "",
+    title: "",
+    author: "",
+    isbn: "",
+    publisher: "",
+    year: "",
+    genre: "",
+    price: "",
+  });
+
+  const handleAddBook = async () => {
+    try {
+      await addBook(newBook);
+      // Close the modal and trigger the parent's onAddBook function
+      onAddBook();
+      setNewBook({
+        bookID: "",
+        title: "",
+        author: "",
+        isbn: "",
+        publisher: "",
+        year: "",
+        genre: "",
+        price: "",
+      });
+      onClose();
+    } catch (error) {
+      console.error("Error adding book:", error);
+      // Handle error (e.g., show an error message to the user)
+    }
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div style={modalOverlayStyle} className="z-[9999]">
+          <div style={modalPopupStyle}>
+            <h2
+              style={{
+                fontweight: "bold",
+                fontSize: "24px",
+                marginBottom: "15px",
+              }}
+            >
+              Menambahkan Buku
+            </h2>
+
+            {/* Input fields */}
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Book ID</label>
+              <input
+                type="text"
+                value={newBook.bookID}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, bookID: e.target.value })
+                }
+                style={inputStyle}
+              />
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Judul Buku</label>
+              <input
+                type="text"
+                value={newBook.title}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, title: e.target.value })
+                }
+                style={inputStyle}
+              />
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Pengarang</label>
+              <input
+                type="text"
+                value={newBook.author}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, author: e.target.value })
+                }
+                style={inputStyle}
+              />
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>ISBN</label>
+              <input
+                type="text"
+                value={newBook.isbn}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, isbn: e.target.value })
+                }
+                style={inputStyle}
+              />
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Penerbit</label>
+              <input
+                type="text"
+                value={newBook.publisher}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, publisher: e.target.value })
+                }
+                style={inputStyle}
+              />
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Tahun Terbit</label>
+              <input
+                type="text"
+                value={newBook.year}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, year: e.target.value })
+                }
+                style={inputStyle}
+              />
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Genre</label>
+              <select
+                value={newBook.genre}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, genre: e.target.value })
+                }
+                style={inputStyle}
+              >
+                <option value="" disabled>
+                  Select a genre
+                </option>
+                {predefinedGenres.map((genre) => (
+                  <option key={genre} value={genre}>
+                    {genre}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Harga</label>
+              <input
+                type="text"
+                value={newBook.price}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, price: e.target.value })
+                }
+                style={inputStyle}
+              />
+            </div>
+
+            {/* Button Container */}
+            <div style={buttonContainerStyle}>
+              {/* Cancel Button */}
+              <div style={cancelButtonStyle} onClick={onClose}>
+                Batal
+              </div>
+              {/* Submit Button */}
+              <div style={submitButtonStyle} onClick={handleAddBook}>
+                Tambah
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AddBookModal;
---
+++ frontend/components/addRowModal.js
@@ -0,0 +1,195 @@
+import { useEffect, useState } from "react";
+import Button from "@/components/Button";
+import { fetchBooks } from "@/lib/getBook";
+import { getNextTransactionId } from "@/lib/getTransaction";
+
+const modalOverlayStyle = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  background: "rgba(0, 0, 0, 0.5)",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const modalPopupStyle = {
+  background: "#fff",
+  borderRadius: "8px",
+  boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+  maxWidth: "400px",
+  width: "100%",
+  maxHeight: "70vh", // Set your desired maximum height
+  overflowY: "auto", // Enable vertical scrolling if content overflows
+  padding: "20px",
+};
+
+const buttonContainerStyle = {
+  marginTop: "20px", // Adjust the margin to control the spacing
+  display: "flex",
+  justifyContent: "flex-end", // Align buttons to the end of the container
+};
+
+const buttonStyle = {
+  borderRadius: "8px",
+  padding: "10px 20px",
+  cursor: "pointer",
+};
+
+const cancelButtonStyle = {
+  ...buttonStyle,
+  marginRight: "10px", // Adjust the margin between buttons
+  background: "#fd4d3e",
+  color: "#fff",
+};
+
+const submitButtonStyle = {
+  ...buttonStyle,
+  background: "#18ef61",
+  color: "#fff",
+};
+
+const labelStyle = {
+  marginBottom: "5px",
+  fontweight: "bold",
+};
+
+const inputContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  marginBottom: "15px",
+};
+
+const inputStyle = {
+  width: "100%", // Set to your desired width
+  padding: "10px",
+  borderRadius: "8px",
+  boxSizing: "border-box",
+  border: "1px solid #ccc",
+};
+
+const AddRowModal = ({ isOpen, onClose, onAddRow }) => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      try {
+        const bookData = await fetchBooks();
+        setBooks(bookData);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        // Handle error (e.g., show an error message to the user)
+      }
+    };
+
+    if (isOpen) {
+      fetchBook();
+    }
+  }, [isOpen]);
+
+  const [newRow, setNewRow] = useState({
+    idTransaction: "",
+    title: "",
+    quantity: "",
+    price: "",
+    totalPrice: "",
+  });
+
+  const handleTitleChange = (e) => {
+    const selectedTitle = e.target.value;
+    const selectedBook = books.find((book) => book.title === selectedTitle);
+
+    setNewRow({
+      ...newRow,
+      title: selectedTitle,
+      price: selectedBook ? selectedBook.price : "", // Set the price based on the selected title
+    });
+  };
+
+  const handleAddRow = () => {
+    try {
+      onAddRow(newRow);
+      setNewRow({
+        idTransaction: newRow.idTransaction,
+        title: newRow.title,
+        quantity: newRow.quantity,
+        price: newRow.price,
+        totalPrice: newRow.totalPrice,
+      });
+      onClose();
+    } catch (error) {
+      console.error("Error adding row:", error);
+      // Handle error (e.g., show an error message to the user)
+    }
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div style={modalOverlayStyle}>
+          <div style={modalPopupStyle}>
+            <h2
+              style={{
+                fontweight: "bold",
+                fontSize: "24px",
+                marginBottom: "15px",
+              }}
+            >
+              Menambahkan Baris
+            </h2>
+
+            <div style={inputContainerStyle}>
+              <label>Judul Buku</label>
+              <select
+                value={books.title}
+                onChange={handleTitleChange}
+                style={inputStyle}
+              >
+                <option value="" disabled selected>
+                  Select a book
+                </option>
+                {books.map((book) => (
+                  <option key={book.bookID} value={book.title}>
+                    {book.title}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div style={inputContainerStyle}>
+              <label style={labelStyle}>Jumlah</label>
+              <input
+                type="number"
+                value={newRow.quantity >= 0 ? newRow.quantity : 0}
+                onChange={(e) => {
+                  const inputValue = parseInt(e.target.value, 10);
+                  const validQuantity = isNaN(inputValue)
+                    ? 0
+                    : Math.max(0, inputValue);
+                  setNewRow({ ...newRow, quantity: validQuantity });
+                }}
+                style={inputStyle}
+              />
+            </div>
+
+            {/* Button Container */}
+            <div style={buttonContainerStyle}>
+              {/* Cancel Button */}
+              <div style={cancelButtonStyle} onClick={onClose}>
+                Batal
+              </div>
+              {/* Submit Button */}
+              <div style={submitButtonStyle} onClick={handleAddRow}>
+                Tambah
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AddRowModal;
